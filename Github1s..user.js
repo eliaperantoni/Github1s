40c13022b25761dca335cd6866e0c96249134533
JackieZheng <zhengxinzhe@admans.net>
2021-02-10 15:25:34 +0800

    Create Github1s..user.js

---
+++ Github1s..user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @namespace    https://github.com/JackieZheng
+// @name         Github1s
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  One second to read GitHub code with VS Code.
+// @author       JackieZheng
+// @match        https://github.com/*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var github1sBtn = document.createElement("li");
+    github1sBtn.innerHTML ='Github1s';
+    github1sBtn.className='open-on-sourcegraph btn btn-sm tooltipped tooltipped-s';
+    var li=document.getElementsByClassName('pagehead-actions flex-shrink-0 d-none d-md-inline').item(0).getElementsByTagName('li')[0]
+    document.getElementsByClassName('pagehead-actions flex-shrink-0 d-none d-md-inline').item(0).insertBefore(github1sBtn,li)
+    github1sBtn.onclick=function()
+    {
+        var href=top.location.href.replace('github.com','github1s.com')
+        top.location.href=href;
+    }
+})();
