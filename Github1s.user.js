c302743691c223506af1a942d45160f5586b6445
JackieZheng <zhengxinzhe@admans.net>
2023-07-21 14:25:23 +0800

    Update Github1s.user.js

--- Github1s.user.js
+++ Github1s.user.js
@@ -1,23 +1,27 @@
 // ==UserScript==
-// @namespace    https://github.com/JackieZheng/Github1s
 // @name         Github1s
-// @namespace    https://github.com/conwnet/github1s
-// @version      0.4
+// @namespace    https://github.com/JackieZheng/Github1s
+// @version      0.5
 // @description  One second to read GitHub code with VS Code.
 // @author       JackieZheng
 // @match        https://github.com/*/*
 // @supportURL   https://github.com/JackieZheng/Github1s/issues
 // @icon         https://github1s.com/favicon.ico
-// @grant        none
+// @grant        GM_addStyle
 // ==/UserScript==
+
+GM_addStyle("anchored-position{display: none !important}}");
+GM_addStyle("anchored-position[class$=popover-open] {display: block !important}}");
 
 (function() {
     'use strict';
     var github1sBtn = document.createElement("li");
     github1sBtn.innerHTML ='Github1s';
     github1sBtn.className='open-on-sourcegraph btn btn-sm tooltipped tooltipped-s';
-    var li=document.getElementsByClassName('pagehead-actions flex-shrink-0 d-none d-md-inline').item(0).getElementsByTagName('li')[0]
-    document.getElementsByClassName('pagehead-actions flex-shrink-0 d-none d-md-inline').item(0).insertBefore(github1sBtn,li)
+    github1sBtn.style.color='var(--color-btn-primary-text)';
+    github1sBtn.style.backgroundColor='var(--color-btn-primary-bg)';
+    var li=document.getElementsByClassName('pagehead-actions flex-shrink-0 d-none d-md-inline').item(0)?.getElementsByTagName('li')[0]
+    document.getElementsByClassName('pagehead-actions flex-shrink-0 d-none d-md-inline').item(0)?.insertBefore(github1sBtn,li)
     github1sBtn.onclick=function()
     {
         var href=top.location.href.replace('github.com','github1s.com')
